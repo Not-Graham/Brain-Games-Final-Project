b10ddfb130be7e885cf8fc7edf9dd771018e4483
Not-Graham <86011515+Not-Graham@users.noreply.github.com>
2022-01-03 20:47:05 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,2 +1,6 @@
 import os
+os.system("npm install -g npm")
+os.system("npm install -g localtunnel")
+os.system("npm i --package-lock-only")
+os.system("npm audit fix")
 os.system("lt -p 5000 -s grahamswebsite")
