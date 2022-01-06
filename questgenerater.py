17b0d39ed719ed50402102105d3eaaa0bd3faac2
Not-Graham <86011515+Not-Graham@users.noreply.github.com>
2022-01-05 20:46:26 -0500

    Update questgenerater.py

--- questgenerater.py
+++ questgenerater.py
@@ -24,34 +24,3 @@ import requests
 from bs4 import BeautifulSoup
 import random
 newline = '\n'
-def generate_article: 
-    url = requests.get("https://en.wikipedia.org/wiki/Special:Random")
-    soup = BeautifulSoup(url.content, "html.parser")
-    title = soup.find(class_="firstHeading").text
-    try:
-      summary = wikipedia.summary(title)
-    except wikipedia.exceptions.DisambiguationError:
-      continue
-    except wikipedia.exceptions.PageError:
-      continue
-    length = len(summary)
-    if length >= 1000 or length <= 500:
-      continue
-    print(f"Title of the article: {title}. The article is {length} characters long. {newline}Do you want to be quizzed on it? (Y/N) After 1 pass you will lose points.")
-    ans = input("").lower()
-    #print("summary type: ", type(summary))
-    if ans == "y" or ans == "yes":
-        qe = main.BoolQGen()
-        payload = {
-          "input_text": summary
-          }
-        pprint("Text/Reading: " + summary)
-        output = qe.predict_boolq(payload)
-        sleep(5)
-        pprint("Question: " + random.choice(output['Boolean Questions']))
-        
-        #print("raw output: ", output)
-        #qg = main.QGen()
-        #output = qg.predict_mcq(payload)
-        #pprint (output)
-#30,000 ms(max of 30 seconds to answer)
