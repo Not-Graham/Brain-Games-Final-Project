bd53b5cd027723e8a6020f5b3258f6d254c06b3b
Not-Graham <86011515+Not-Graham@users.noreply.github.com>
2022-01-05 19:53:27 -0500

    Update ajax.py

--- ajax.py
+++ ajax.py
@@ -1,23 +1,24 @@
 import numpy
 from pprint import pprint
+from time import sleep
 import nltk
 nltk.download('stopwords')
 try:
   from Questgen import main
 except:
-  !pip install git+https://github.com/ramsrigouthamg/Questgen.ai
-  !pip install git+https://github.com/boudinfl/pke.git
-  !python -m nltk.downloader universal_tagset
-  !python -m spacy download en
+  os.system("pip install git+https://github.com/ramsrigouthamg/Questgen.ai")
+  os.system("pip install git+https://github.com/boudinfl/pke.git")
+  os.system("python -m nltk.downloader universal_tagset")
+  os.system("python -m spacy download en")
   from Questgen import main
 file_exists = exists("/content/s2v_reddit_2015_md.tar.gz")
 if file_exists != True:
-  !wget https://github.com/explosion/sense2vec/releases/download/v1.0.0/s2v_reddit_2015_md.tar.gz
-  !tar -xvf  s2v_reddit_2015_md.tar.gz
+  os.system("wget https://github.com/explosion/sense2vec/releases/download/v1.0.0/s2v_reddit_2015_md.tar.gz")
+  os.system("tar -xvf  s2v_reddit_2015_md.tar.gz")
 try:
   import wikipedia
 except:
-  !pip install wikipedia
+  os.system("pip install wikipedia")
   import wikipedia
 import requests
 from bs4 import BeautifulSoup
@@ -25,8 +26,7 @@ import random
 newline = '\n'
 
 #computational code goes past this point
-
-for x in range(0, 4): 
+def generate_article+: 
     url = requests.get("https://en.wikipedia.org/wiki/Special:Random")
     soup = BeautifulSoup(url.content, "html.parser")
     title = soup.find(class_="firstHeading").text
@@ -49,18 +49,11 @@ for x in range(0, 4):
           }
         pprint("Text/Reading: " + summary)
         output = qe.predict_boolq(payload)
+        sleep(5)
         pprint("Question: " + random.choice(output['Boolean Questions']))
         
         #print("raw output: ", output)
-        #print("length of summary :", length)
         #qg = main.QGen()
         #output = qg.predict_mcq(payload)
         #pprint (output)
-    
-    elif ans == "n" or ans == "no":
-        print("Try Again!")
-        continue
-    elif ans == "e" or ans == "explicit":
-        print("Try Again!")
-        continue
 #30,000 ms(max of 30 seconds to answer)
